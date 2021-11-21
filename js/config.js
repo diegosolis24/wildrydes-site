b97e63b257a66cde9903762102dd33abaefbe2e6
Diego Solis <solisdiego2400@gmail.com>
2021-11-21 14:13:13 -0600

    hehehehhehe we are doing itttttt

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_x2RxXoj5c', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '75rnogmrlifj26d2g0isb09r4m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
